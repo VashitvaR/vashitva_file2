dacf5eccf435af1abc07da6851dc38f45080e1c6
VashitvaR <vashitva.bagga@gmail.com>
2024-03-29 18:54:42 +0530

    cc

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import pdfplumber
 import openai
 from dotenv import load_dotenv
 import streamlit as st
-from PyPDF2 import PdfReader
+
 from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
